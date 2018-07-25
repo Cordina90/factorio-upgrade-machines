20097f883befaf48f98c342fbf797df27ea56a42
Jelmergu <jelmergujelmer@hotmail.com>
2018-07-25 21:59:00 +0200

    Add burner-to-electric-drill to data-updates
    Signed-off-by: Jelmergu <jelmergujelmer@hotmail.com>
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

--- upgrade-machines/prototypes/recipes/vanilla.lua
+++ upgrade-machines/prototypes/recipes/vanilla.lua
@@ -34,88 +34,158 @@ Jelmergu:addRecipe({
 })
 
 Jelmergu:addRecipe(
-    {
-        name = "burner-to-electric-inserter",
-        expensive = false,
-        ingredients = {
-            { "burner-inserter", 1 },
-            { "electronic-circuit", 1 }
-        },
-        result = "inserter",
-        energy_required = 1,
-        enabled = true
-    }
+        {
+            name = "burner-to-electric-inserter",
+            expensive = false,
+            ingredients = {
+                { "burner-inserter", 1 },
+                { "electronic-circuit", 1 }
+            },
+            result = "inserter",
+            energy_required = 1,
+            enabled = true
+        }
 )
 
 Jelmergu:addRecipe(
-    {
-        name = "stone-to-steel-furnace",
-        expensive = false,
-        ingredients = {
-            { "stone-furnace", 1 },
-            { "steel-plate", 6 },
-            { "stone-brick", 9 }
-        },
-        result = "steel-furnace",
-        energy_required = 3,
-    }
+        {
+            name = "stone-to-steel-furnace",
+            expensive = false,
+            ingredients = {
+                { "stone-furnace", 1 },
+                { "steel-plate", 6 },
+                { "stone-brick", 9 }
+            },
+            result = "steel-furnace",
+            energy_required = 3,
+        }
 )
 
 Jelmergu:addRecipe(
-    {
-        name = "steel-to-electric-furnace",
-        expensive = false,
-        ingredients = {
-            { "steel-furnace", 1 },
-            { "steel-plate", 4 },
-            { "advanced-circuit", 5 }
-        },
-        result = "electric-furnace",
-        energy_required = 5,
-    }
+        {
+            name = "steel-to-electric-furnace",
+            expensive = false,
+            ingredients = {
+                { "steel-furnace", 1 },
+                { "steel-plate", 4 },
+                { "advanced-circuit", 5 }
+            },
+            result = "electric-furnace",
+            energy_required = 5,
+        }
 )
 
 Jelmergu:addRecipe(
-    {
-        name = "pistol-to-submachine",
-        ingredients = {
-            { "pistol", 1},
-            { "iron-plate", 5},
-            { "iron-gear-wheel", 10}
-        },
-        result = "submachine-gun",
-        energy_required = 10,
-    }
+        {
+            name = "pistol-to-submachine",
+            ingredients = {
+                { "pistol", 1 },
+                { "iron-plate", 5 },
+                { "iron-gear-wheel", 10 }
+            },
+            result = "submachine-gun",
+            energy_required = 10,
+        }
 )
 
 Jelmergu:addRecipe(
-    {
-        name = "pistol-to-shotgun",
-        ingredients = {
-            { "pistol", 1},
-            { "iron-plate", 10},
-            { "copper-plate", 5},
-            { "iron-gear-wheel", 5},
-            { "wood", 5}
+        {
+            name = "pistol-to-shotgun",
+            ingredients = {
+                { "pistol", 1 },
+                { "iron-plate", 10 },
+                { "copper-plate", 5 },
+                { "iron-gear-wheel", 5 },
+                { "wood", 5 }
 
-        },
-        result = "shotgun",
-        energy_required = 10,
-    }
+            },
+            result = "shotgun",
+            energy_required = 10,
+        }
 )
 
 Jelmergu:addRecipe(
-    {
-        name = "pistol-to-combat-shotgun",
-        ingredients = {
-            { "pistol", 1},
-            { "iron-plate", 5},
-            { "copper-plate", 5},
-            { "steel-plate", 15},
-            { "wood", 10},
-        },
-        result = "combat-shotgun",
-        energy_required = 10,
-    }
+        {
+            name = "pistol-to-combat-shotgun",
+            ingredients = {
+                { "pistol", 1 },
+                { "iron-plate", 5 },
+                { "copper-plate", 5 },
+                { "steel-plate", 15 },
+                { "wood", 10 },
+            },
+            result = "combat-shotgun",
+            energy_required = 10,
+        }
+)
+
+Jelmergu:addRecipe(
+        {
+            name = "modular-to-power-armor",
+            ingredients = {
+                {"modular-armor", 1},
+                {"electric-engine-unit", 20},
+                {"processing-unit", 25},
+                {"electronic-circuit", 300},
+            },
+            results = {
+                {"power-armor", 1},
+                {"steel-plate", 10}
+            },
+            expensive = false,
+            energy_required = 20
+        }
+)
+
+Jelmergu:addRecipe(
+        {
+            name = "power-to-power-armor-mk2",
+            ingredients = {
+                {"power-armor", 1},
+                {"effectivity-module-3", 5},
+                {"speed-module-3", 5}
+            },
+            results = {
+                {"power-armor-mk2", 1},
+                {"electric-engine-unit", 20},
+            },
+            expensive = false,
+            energy_required = 25
+
+        }
+)
+
+Jelmergu:addRecipe(
+        {
+            name = "light-to-heavy-armor",
+            ingredients = {
+                {"light-armor", 1},
+                {"copper-plate", 100},
+                {"steel-plate", 50}
+            },
+            results = {
+                {"heavy-armor", 1},
+                {"iron-plate", 40},
+            },
+            expensive = false,
+            energy_required = 8
+
+        }
+)
+
+Jelmergu:addRecipe(
+        {
+            name = "heavy-to-modular-armor",
+            ingredients = {
+                {"heavy-armor", 1},
+                {"advanced-circuit", 30},
+            },
+            results = {
+                {"modular-armor", 1},
+                {"copper-plate", 100},
+            },
+            expensive = false,
+            energy_required = 15
+        }
 )
 
--- upgrade-machines/prototypes/technology/vanilla.lua
+++ upgrade-machines/prototypes/technology/vanilla.lua
@@ -17,3 +17,23 @@ table.insert(data.raw["technology"]["advanced-material-processing-2"].effects, {
     type = "unlock-recipe",
     recipe = "steel-to-electric-furnace"
 })
+
+table.insert(data.raw["technology"]["heavy-armor"].effects, {
+    type = "unlock-recipe",
+    recipe = "light-to-heavy-armor"
+})
+
+table.insert(data.raw["technology"]["modular-armor"].effects, {
+    type = "unlock-recipe",
+    recipe = "heavy-to-modular-armor"
+})
+
+table.insert(data.raw["technology"]["power-armor"].effects, {
+    type = "unlock-recipe",
+    recipe = "modular-to-power-armor"
+})
+
+table.insert(data.raw["technology"]["power-armor-2"].effects, {
+    type = "unlock-recipe",
+    recipe = "power-to-power-armor-mk2"
+})
