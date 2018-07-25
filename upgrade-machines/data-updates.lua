1268d5d62b14e1e8020c911cd3ca17ba909aedb2
Jelmergu <jelmergujelmer@hotmail.com>
2018-07-25 21:59:00 +0200

    Add burner-to-electric-drill to data-updates
    Signed-off-by: Jelmergu <jelmergujelmer@hotmail.com>

--- upgrade-machines/data-updates.lua
+++ upgrade-machines/data-updates.lua
@@ -3,5 +3,11 @@ if data.raw.recipe["basic-circuit-board"] then
         { "basic-circuit-board", 1 },
         { "burner-inserter", 1 }
     }
+    data.raw.recipe["burner-to-electric-drill"].ingredients = {
+        { "burner-mining-drill", 1 },
+        { "iron-gear-wheel", 2 },
+        { "iron-plate", 7 },
+        { "basic-circuit-board", 3},
+    }
 end
 
