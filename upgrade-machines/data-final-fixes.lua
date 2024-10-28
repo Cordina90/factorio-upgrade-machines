432836d565bb03ae892eb71a1f4dc7956b738690
Cordina90 <pierbraaksma@hotmail.com>
2024-10-28 12:52:03 +0100

    Update to factorio 2.0

--- upgrade-machines/data-final-fixes.lua
+++ upgrade-machines/data-final-fixes.lua
@@ -1,20 +1,20 @@
 if data.raw.recipe["basic-circuit-board"] and data.raw.recipe["burner-to-electric-inserter"] then
     data.raw.recipe["burner-to-electric-inserter"].ingredients = {
-        { "basic-circuit-board", 1 },
-        { "burner-inserter", 1 }
+        { type = "item", name = "basic-circuit-board", amount = 1 },
+        { type = "item", name = "burner-inserter", amount = 1 }
     }
 end
 if data.raw.recipe["basic-circuit-board"] and data.raw.recipe["burner-to-electric-drill"] then
     data.raw.recipe["burner-to-electric-drill"].normal.ingredients = {
-        { "burner-mining-drill", 1 },
-        { "iron-gear-wheel", 2 },
-        { "iron-plate", 7 },
-        { "basic-circuit-board", 3},
+        { type = "item", name = "burner-mining-drill", amount = 1 },
+        { type = "item", name = "iron-gear-wheel", amount = 2 },
+        { type = "item", name = "iron-plate", amount = 7 },
+        { type = "item", name = "basic-circuit-board", amount = 3 },
     }
 end
 if data.raw.recipe["basic-circuit-board"] and data.raw.recipe["burner-assembling-to-assembling-machine-1"] then
     data.raw.recipe["burner-assembling-to-assembling-machine-1"].ingredients = {
-        { "burner-assembling-machine", 1 },
-        { "basic-circuit-board", 3 },
+        { type = "item", name = "burner-assembling-machine", amount = 1 },
+        { type = "item", name = "basic-circuit-board", amount = 3 },
     }
 end
