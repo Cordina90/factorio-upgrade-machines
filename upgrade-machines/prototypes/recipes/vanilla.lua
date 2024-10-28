3759b3a666cf9814a1d1906d529b1c3499d799ef
Cordina90 <pierbraaksma@hotmail.com>
2024-10-28 12:43:45 +0100

    Update vanilla.lua to factorio 2.0

--- upgrade-machines/prototypes/recipes/vanilla.lua
+++ upgrade-machines/prototypes/recipes/vanilla.lua
@@ -6,270 +6,258 @@ data:extend({
         order = "z",
     }
 })
-
-Jelmergu:addRecipe({
-    name = "burner-to-electric-drill",
-    enabled = true,
-    expensive = true,
-    ingredients = {
-        { "burner-mining-drill", 1 },
-        { "iron-gear-wheel", 2 },
-        { "iron-plate", 7 },
-        { "electronic-circuit", 3 }
-    },
-    results = {
+Jelmergu:addRecipe(
+    {
+        type = "recipe",
+        name = "burner-to-electric-inserter",
+        enabled = true,
+        energy_required = 0.5,
+        ingredients =
         {
-            type = "item",
-            name = "electric-mining-drill",
-            amount = 1
+            { type = "item", name = "electronic-circuit", amount = 1 },
+            { type = "item", name = "burner-inserter", amount = 1 },
         },
-        {
-            type = "item",
-            name = "stone-furnace",
-            amount = 1
+        results = {
+            { type = "item", name = "inserter", amount = 1 }
+        }
+    }
+)
+Jelmergu:addRecipe(
+    {
+        type = "recipe",
+        name = "burner-to-electric-drill",
+        enabled = true,
+        expensive = true,
+        ingredients = {
+            { type = "item", name = "burner-mining-drill", amount = 1 },
+            { type = "item", name = "iron-gear-wheel", amount = 2 },
+            { type = "item", name = "iron-plate", amount = 7 },
+            { type = "item", name = "electronic-circuit", amount = 3 }
         },
-    },
-    main_product = "electric-mining-drill",
-    energy_required = 2,
-})
-
-Jelmergu:addRecipe(
-        {
-            name = "burner-to-electric-inserter",
-            expensive = false,
-            ingredients = {
-                { "burner-inserter", 1 },
-                { "electronic-circuit", 1 }
+        results = {
+            {
+                type = "item",
+                name = "electric-mining-drill",
+                amount = 1
             },
-            result = "inserter",
-            energy_required = 0.5,
-            enabled = true
-        }
+            {
+                type = "item",
+                name = "stone-furnace",
+                amount = 1
+            },
+        },
+        main_product = "electric-mining-drill",
+        energy_required = 2,
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "stone-to-steel-furnace",
-            expensive = false,
-            ingredients = {
-                { "stone-furnace", 1 },
-                { "steel-plate", 6 },
-                { "stone-brick", 9 }
-            },
-            result = "steel-furnace",
-            energy_required = 3,
-        }
+    {
+        type = "recipe",
+        name = "stone-to-steel-furnace",
+        ingredients = {
+            { type = "item", name = "steel-plate", amount = 6 },
+            { type = "item", name = "stone-brick", amount = 8 },
+            { type = "item", name = "stone-furnace", amount = 1 }
+        },
+        results = {
+            { type = "item", name = "steel-furnace", amount = 1 }
+        },
+        energy_required = 3,
+        enabled = false
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "steel-to-electric-furnace",
-            expensive = false,
-            ingredients = {
-                { "steel-furnace", 1 },
-                { "steel-plate", 4 },
-                { "advanced-circuit", 5 }
-            },
-            result = "electric-furnace",
-            energy_required = 5,
-        }
+    {
+        type = "recipe",
+        name = "steel-to-electric-furnace",
+        ingredients = {
+            { type = "item", name = "steel-furnace", amount = 1 },
+            { type = "item", name = "steel-plate", amount = 4 },
+            { type = "item", name = "advanced-circuit", amount = 5 }
+        },
+        results = {
+            { type = "item", name = "electric-furnace", amount = 1 }
+        },
+        energy_required = 5,
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "pistol-to-submachine",
-            ingredients = {
-                { "pistol", 1 },
-                { "iron-plate", 5 },
-                { "iron-gear-wheel", 10 }
-            },
-            result = "submachine-gun",
-            energy_required = 10,
-        }
+    {
+        type = "recipe",
+        name = "pistol-to-submachine",
+        ingredients = {
+            { type = "item", name = "pistol", amount = 1 },
+            { type = "item", name = "iron-plate", amount = 5 },
+            { type = "item", name = "iron-gear-wheel", amount = 10 }
+        },
+        results = { 
+            { type = "item", name = "submachine-gun", amount = 1 }
+        },
+        energy_required = 10,
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "pistol-to-shotgun",
-            ingredients = {
-                { "pistol", 1 },
-                { "iron-plate", 10 },
-                { "copper-plate", 5 },
-                { "iron-gear-wheel", 5 },
-                { "wood", 5 }
+    {
+        type = "recipe",
+        name = "pistol-to-shotgun",
+        ingredients = {
+            { type = "item", name = "pistol", amount = 1 },
+            { type = "item", name = "iron-plate", amount = 10 },
+            { type = "item", name = "copper-plate", amount = 5 },
+            { type = "item", name = "iron-gear-wheel", amount = 5 },
+            { type = "item", name = "wood", amount = 5 }
 
-            },
-            result = "shotgun",
-            energy_required = 10,
-        }
+        },
+        results = {
+            { type = "item", name = "shotgun", amount = 1 }
+        },
+        energy_required = 10,
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "shotgun-to-combat-shotgun",
-            ingredients = {
-                { "shotgun", 1 },
-                { "steel-plate", 14 },
-            },
-            result = "combat-shotgun",
-            energy_required = 10,
-        }
+    {
+        type = "recipe",
+        name = "shotgun-to-combat-shotgun",
+        ingredients = {
+            { type = "item", name = "shotgun", amount = 1 },
+            { type = "item", name = "steel-plate", amount = 14 },
+        },
+        results = {
+            { type = "item", name = "combat-shotgun", amount = 1 }
+        },
+        energy_required = 10,
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "light-to-heavy-armor",
-            ingredients = {
-                {"light-armor", 1},
-                {"copper-plate", 100},
-                {"steel-plate", 50}
-            },
-            results = {
-                {"heavy-armor", 1},
-                {"iron-plate", 40},
-            },
-            expensive = false,
-            energy_required = 8
+    {
+        type = "recipe",
+        name = "light-to-heavy-armor",
+        ingredients = {
+            { type = "item", name = "light-armor", amount = 1 },
+            { type = "item", name = "copper-plate", amount = 100 },
+            { type = "item", name = "steel-plate", amount = 50 }
+        },
+        results = {
+            { type = "item", name = "heavy-armor", amount = 1 },
+            { type = "item", name = "iron-plate", amount = 40 },
+        },
+        energy_required = 8
 
-        }
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "heavy-to-modular-armor",
-            ingredients = {
-                {"heavy-armor", 1},
-                {"advanced-circuit", 30},
-            },
-            results = {
-                {"modular-armor", 1},
-                {"copper-plate", 100},
-            },
-            expensive = false,
-            energy_required = 15
-        }
+    {
+        type = "recipe",
+        name = "heavy-to-modular-armor",
+        ingredients = {
+            { type = "item", name = "heavy-armor", amount = 1 },
+            { type = "item", name = "advanced-circuit", amount = 30 },
+        },
+        results = {
+            { type = "item", name = "modular-armor", amount = 1 },
+            { type = "item", name = "copper-plate", amount = 100 },
+        },
+        energy_required = 15
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "modular-to-power-armor",
-            ingredients = {
-                {"modular-armor", 1},
-                {"electric-engine-unit", 20},
-                {"processing-unit", 25},
-                {"electronic-circuit", 300},
-            },
-            results = {
-                {"power-armor", 1},
-                {"steel-plate", 10}
-            },
-            expensive = false,
-            energy_required = 20
-        }
+    {
+        type = "recipe",
+        name = "modular-to-power-armor",
+        ingredients = {
+            { type = "item", name = "modular-armor",        amount = 1 },
+            { type = "item", name = "electric-engine-unit", amount = 20 },
+            { type = "item", name = "processing-unit",      amount = 25 },
+            { type = "item", name = "electronic-circuit",   amount = 300 },
+        },
+        results = {
+            { type = "item", name = "power-armor", amount = 1 },
+            { type = "item", name = "steel-plate", amount = 10 }
+        },
+        energy_required = 20
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "power-to-power-armor-mk2",
-            ingredients = {
-                {"power-armor", 1},
-                {"effectivity-module-3", 5},
-                {"speed-module-3", 5}
-            },
-            results = {
-                {"power-armor-mk2", 1},
-                {"electric-engine-unit", 20},
-            },
-            expensive = false,
-            energy_required = 25
-
-        }
+    {
+        type = "recipe",
+        name = "power-to-power-armor-mk2",
+        ingredients = {
+            { type = "item", name = "power-armor", amount = 1 },
+            { type = "item", name = "efficiency-module-2", amount = 5 },
+            { type = "item", name = "speed-module-2", amount = 5 }
+        },
+        results = {
+            { type = "item", name = "power-armor-mk2", amount = 1 },
+            { type = "item", name = "electric-engine-unit", amount = 20 },
+        },
+        energy_required = 25
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "light-to-heavy-armor",
-            ingredients = {
-                {"light-armor", 1},
-                {"copper-plate", 100},
-                {"steel-plate", 50}
-            },
-            results = {
-                {"heavy-armor", 1},
-                {"iron-plate", 40},
-            },
-            expensive = false,
-            energy_required = 8
-
-        }
+    {
+        type = "recipe",
+        name = "light-to-heavy-armor",
+        ingredients = {
+            { type = "item", name = "light-armor", amount = 1 },
+            { type = "item", name = "copper-plate", amount = 100 },
+            { type = "item", name = "steel-plate", amount = 50 }
+        },
+        results = {
+            { type = "item", name = "heavy-armor", amount = 1 },
+            { type = "item", name = "iron-plate", amount = 40 },
+        },
+        energy_required = 8
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "heavy-to-modular-armor",
-            ingredients = {
-                {"heavy-armor", 1},
-                {"advanced-circuit", 30},
-            },
-            results = {
-                {"modular-armor", 1},
-                {"copper-plate", 100},
-            },
-            expensive = false,
-            energy_required = 15
-        }
+    {
+        type = "recipe",
+        name = "heavy-to-modular-armor",
+        ingredients = {
+            { type = "item", name = "heavy-armor", amount = 1 },
+            { type = "item", name = "advanced-circuit", amount = 30 },
+        },
+        results = {
+            { type = "item", name = "modular-armor", amount = 1 },
+            { type = "item", name = "copper-plate", amount = 100 },
+        },
+        energy_required = 15
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "filter-inserter-to-stack-filter-inserter",
-            ingredients = {
-                {"filter-inserter", 1},
-                {"advanced-circuit", 1},
-                {"electronic-circuit", 16},
-                {"iron-gear-wheel", 15}
+    {
+        type = "recipe",
+        name = "boiler-to-heat-exchanger",
+        ingredients = {
+            { type = "item", name = "boiler", amount = 2 },
+            { type = "item", name = "pipe", amount = 2 },
+            { type = "item", name = "copper-plate", amount = 100 },
+            { type = "item", name = "steel-plate", amount = 10 }
 
-            },
-            icon_size = 64,
-            result = "stack-filter-inserter",
-            expensive = false,
-            energy_required = 3
-        }
+        },
+        results = {
+            { type = "item", name = "heat-exchanger", amount = 1 },
+            { type = "item", name = "stone-furnace", amount = 2 },
+        },
+        icon_size = 64,
+        energy_required = 3
+    }
 )
-
 Jelmergu:addRecipe(
-        {
-            name = "boiler-to-heat-exchanger",
-            ingredients = {
-                {"boiler", 2},
-                {"pipe", 2},
-                {"copper-plate", 100},
-                {"steel-plate", 10}
+    {
+        type = "recipe",
+        name = "steam-engine-to-steam-turbine",
+        ingredients = {
+            { type = "item", name = "steam-engine", amount = 3 },
+            { type = "item", name = "pipe", amount = 5 },
+            { type = "item", name = "copper-plate", amount = 50 },
+            { type = "item", name = "iron-gear-wheel", amount = 11 }
 
-            },
-            results = {
-                {"heat-exchanger", 1},
-                {"stone-furnace", 2},
-            },
-            expensive = false,
-            icon_size = 64,
-            energy_required = 3
-        }
-)
-
-Jelmergu:addRecipe(
-        {
-            name = "steam-engine-to-steam-turbine",
-            ingredients = {
-                {"steam-engine", 3},
-                {"pipe", 5},
-                {"copper-plate", 50},
-                {"iron-gear-wheel", 11}
-
-            },
-            icon_size = 64,
-            result = "steam-turbine",
-            expensive = false,
-            energy_required = 3
-        }
+        },
+        icon_size = 64,
+        results = {
+            { type = "item", name = "steam-turbine", amount = 1 }
+        },
+        energy_required = 3
+    }
 )
